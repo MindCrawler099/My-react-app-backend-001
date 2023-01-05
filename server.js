e11119be615695cb3d5e9a3edc2bd2f310683c56
MindCrawler099 <botslayer466@gmail.com>
2023-01-05 11:04:46 +0000

    Added cors to app
    Created a corsConfig function and implemented it into the app(server.js) as middleware

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const corsConfig = (req,callback)=>{
+    blackList = [];
+    if(blackList.indexOf(req.header('Origin'))!==1 || !req.header('Origin')){
+        callback(null,true);
+    }else{
+        callback(new Error('There wa san error with xss'),false);
+    }
+}
 
 const app = express();
+app.use(cors(corsConfig));
+
 const port = 4000;
 
 app.listen(port,()=>{
