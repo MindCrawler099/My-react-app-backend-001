e8651d061c2e0fd57c3c43d294fe7108591eb717
MindCrawler099 <botslayer466@gmail.com>
2023-01-05 12:45:50 +0000

    Successfully implemented mysql insertion to database
    The react frontend can now send json details and this server can store the values in a `commentstable` table within a `reactjs001commentsdb` database

--- router/postCommentRouter.js
+++ router/postCommentRouter.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const postCommentRouter = express.Router();
 
 postCommentRouter.use(express.urlencoded({
@@ -6,12 +8,41 @@ postCommentRouter.use(express.urlencoded({
 }));
 postCommentRouter.use(express.json());
 
+const mysqlconnector = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'reactjs001commentsdb'
+});
+
+mysqlconnector.connect((error)=>{
+    if(error){
+        throw Error('The database could not be connected to');
+    }else{
+        console.log('Connected to database');
+    }
+});
+
 postCommentRouter.route('/').post((req,res)=>{
-    const dummyresponse = `${req.body.title} and ${req.body.text}`;
+    const dummyresponse = `${req.body.title} and ${req.body.text} and ${req.body.time}`;
     console.log(dummyresponse);
+
+    // const createTableQuery = 'CREATE TABLE IF NOT EXISTS commentstable( commentid INT AUTO_INCREMENT PRIMARY KEY,commenttitle TEXT NOT NULL,commenttext TEXT NOT NULL,commentdate DATETIME NOT NULL)';
+    // mysqlconnector.query(createTableQuery,(error)=>{
+    //     console.error(`Table could not be created. Also, ${error.message}`);
+    // })
+
+    const insertPostDetailsQuery = `INSERT INTO commentstable(commenttitle,commenttext,commentdate) VALUES ( ? , ? , ? )`;
+    console.log([req.body.title,req.body.text,req.body.time])
+    mysqlconnector.query(insertPostDetailsQuery,[req.body.title,req.body.text,req.body.time],(error,results)=>{
+        if(error){throw error}
+        console.log('Inserted');
+    });
+
     res.json({
         title:req.body.title,
-        text:req.body.text
+        text:req.body.text,
+        time:req.body.time
     })
 })
 
